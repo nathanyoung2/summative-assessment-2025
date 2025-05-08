25e0b6d25b3100a8e1e323bd84e73b5f01f34578
Nathan Young <nathantayloryoung@gmail.com>
2025-05-08 13:35:19 +1200

    Reworked lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,101 +1,93 @@
 #[derive(Debug, Eq, PartialEq)]
 pub enum Token {
-    Command { value: String },
-    Folder { value: String },
-    File { value: String },
+    Word(String),
     PreviousDir,
+    Space,
     Dot,
     Slash,
-    Extension { value: String },
+    And,
 }
 
 /// A `Lexer` is responsible for turning the raw input command from the user into a vector of
 /// tokens that can be interpereted at a later stage.
-pub struct Lexer {
-    input: String,
+pub struct Lexer<'s> {
+    input: &'s str,
+    cursor: usize,
 }
 
-impl Lexer {
+impl<'s> Lexer<'s> {
     /// Create a new `Lexer`.
-    /// Takes in the input command
-    pub fn new(input: &str) -> Self {
-        Self {
-            input: input.to_string(),
-        }
+    /// Takes in the input source text
+    pub fn new(input: &'s str) -> Self {
+        Self { input, cursor: 0 }
     }
 
-    /// Tokenize the input command and return it as a `Vec<Token>`
-    pub fn tokenize(&self) -> Vec<Token> {
+    pub fn tokenize(&mut self) -> Vec<Token> {
         let mut accumulator = Vec::new();
-        // iterate over every command, multiple commands can be combined with `&&`
-        for line in self.input.split("&&") {
-            let parts = line.trim().split_ascii_whitespace().collect::<Vec<_>>();
 
-            // Tokenize the command
-            if let Some(command) = parts.get(0) {
-                accumulator.push(Token::Command {
-                    value: command.to_string(),
-                });
-            }
-
-            // Tokenize the path argument to the command
-            if let Some(path) = parts.get(1) {
-                Self::tokenize_path(&mut accumulator, path);
-            }
+        while let Some(token) = self.read_next_token() {
+            accumulator.push(token);
         }
 
         accumulator
     }
 
-    /// Helper function to tokenize a directory path.
-    /// Takes in an accumulator vector that the resulting tokens will be pushed to as well as the
-    /// path string itself.
-    fn tokenize_path(accumulator: &mut Vec<Token>, path: &str) {
-        // current word index (start of the current word)
-        let mut cwi = 0;
+    fn read_next_token(&mut self) -> Option<Token> {
+        if self.cursor == self.input.len() {
+            return None;
+        }
 
-        // iterate over all characters
-        for (i, c) in path.chars().enumerate() {
-            // if a slash is found, add the folder before the slash
-            // and the slash itself as tokens
-            if c == '/' {
-                let prev_word = path[cwi..i].to_string();
-                accumulator.push(match prev_word.as_str() {
-                    ".." => Token::PreviousDir,
-                    _ => Token::Folder { value: prev_word },
-                });
-                accumulator.push(Token::Slash);
-                cwi = i + 1;
+        if self.check_multi_token("&&") {
+            return Some(Token::And);
+        }
+        if self.check_multi_token("..") {
+            return Some(Token::PreviousDir);
+        }
+
+        match self.input.chars().nth(self.cursor).unwrap() {
+            '.' => {
+                self.cursor += 1;
+                Some(Token::Dot)
+            }
+            '/' => {
+                self.cursor += 1;
+                Some(Token::Slash)
+            }
+            ' ' => {
+                self.cursor += 1;
+                Some(Token::Space)
+            }
+            _ => {
+                let next = self.next_token_index();
+                let word_contents = &self.input[self.cursor..next];
+                self.cursor = next;
+                return Some(Token::Word(word_contents.to_string()));
+            }
+        }
+    }
+
+    fn check_multi_token(&mut self, token: &str) -> bool {
+        if self.cursor + token.len() > self.input.len() {
+            return false;
+        }
+        let chars = &self.input[self.cursor..(self.cursor + token.len())];
+        if chars == token {
+            self.cursor += token.len();
+            return true;
+        }
+
+        false
+    }
+
+    fn next_token_index(&self) -> usize {
+        let chars = self.input[self.cursor..].chars();
+        for (i, c) in chars.enumerate() {
+            if matches![c, '.' | '/' | '&' | ' '] {
+                return self.cursor + i;
             }
         }
 
-        // tokenise file and extension
-        let last = path.split("/").last();
-        if let Some(last) = last {
-            // if there is no `.` found in the last segment of the path it
-            // means that the path ends with a folder
-            if !last.contains(".") {
-                accumulator.push(Token::Folder {
-                    value: last.to_string(),
-                })
-            }
-
-            // otherwise add the file, dot, and extension as a token
-            for (i, c) in last.chars().enumerate() {
-                if c == '.' {
-                    accumulator.push(Token::File {
-                        value: last[0..i].to_string(),
-                    });
-                    accumulator.push(Token::Dot);
-
-                    if let Some(_) = last.chars().collect::<Vec<char>>().get(i + 1) {
-                        accumulator.push(Token::Extension {
-                            value: last[i + 1..].to_string(),
-                        });
-                    }
-                }
-            }
-        }
+        return self.input.len();
     }
 }
 
@@ -108,27 +100,18 @@ mod tests {
         let input = "touch folder1/folder2/file.png";
 
         let expected_tokens = vec![
-            Token::Command {
-                value: String::from("touch"),
-            },
-            Token::Folder {
-                value: String::from("folder1"),
-            },
+            Token::Word(String::from("touch")),
+            Token::Space,
+            Token::Word(String::from("folder1")),
             Token::Slash,
-            Token::Folder {
-                value: String::from("folder2"),
-            },
+            Token::Word(String::from("folder2")),
             Token::Slash,
-            Token::File {
-                value: String::from("file"),
-            },
+            Token::Word(String::from("file")),
             Token::Dot,
-            Token::Extension {
-                value: String::from("png"),
-            },
+            Token::Word(String::from("png")),
         ];
 
-        let command_lexer = Lexer::new(input);
+        let mut command_lexer = Lexer::new(input);
         let tokens = command_lexer.tokenize();
 
         assert_eq!(tokens, expected_tokens);
@@ -139,19 +122,14 @@ mod tests {
         let input = "cd folder1/folder2";
 
         let expected_tokens = vec![
-            Token::Command {
-                value: String::from("cd"),
-            },
-            Token::Folder {
-                value: String::from("folder1"),
-            },
+            Token::Word(String::from("cd")),
+            Token::Space,
+            Token::Word(String::from("folder1")),
             Token::Slash,
-            Token::Folder {
-                value: String::from("folder2"),
-            },
+            Token::Word(String::from("folder2")),
         ];
 
-        let command_lexer = Lexer::new(input);
+        let mut command_lexer = Lexer::new(input);
         let tokens = command_lexer.tokenize();
 
         assert_eq!(tokens, expected_tokens);
@@ -162,29 +140,20 @@ mod tests {
         let input = "cd ../folder1/folder2/file.png";
 
         let expected_tokens = vec![
-            Token::Command {
-                value: String::from("cd"),
-            },
+            Token::Word(String::from("cd")),
+            Token::Space,
             Token::PreviousDir,
             Token::Slash,
-            Token::Folder {
-                value: String::from("folder1"),
-            },
+            Token::Word(String::from("folder1")),
             Token::Slash,
-            Token::Folder {
-                value: String::from("folder2"),
-            },
+            Token::Word(String::from("folder2")),
             Token::Slash,
-            Token::File {
-                value: String::from("file"),
-            },
+            Token::Word(String::from("file")),
             Token::Dot,
-            Token::Extension {
-                value: String::from("png"),
-            },
+            Token::Word(String::from("png")),
         ];
 
-        let command_lexer = Lexer::new(input);
+        let mut command_lexer = Lexer::new(input);
         let tokens = command_lexer.tokenize();
 
         assert_eq!(tokens, expected_tokens);
@@ -195,29 +164,46 @@ mod tests {
         let input = "cd folder1/folder2 && touch file.png";
 
         let expected_tokens = vec![
-            Token::Command {
-                value: String::from("cd"),
-            },
-            Token::Folder {
-                value: String::from("folder1"),
-            },
+            Token::Word(String::from("cd")),
+            Token::Space,
+            Token::Word(String::from("folder1")),
             Token::Slash,
-            Token::Folder {
-                value: String::from("folder2"),
-            },
-            Token::Command {
-                value: String::from("touch"),
-            },
-            Token::File {
-                value: String::from("file"),
-            },
+            Token::Word(String::from("folder2")),
+            Token::Space,
+            Token::And,
+            Token::Space,
+            Token::Word(String::from("touch")),
+            Token::Space,
+            Token::Word(String::from("file")),
             Token::Dot,
-            Token::Extension {
-                value: String::from("png"),
-            },
+            Token::Word(String::from("png")),
         ];
 
-        let command_lexer = Lexer::new(input);
+        let mut command_lexer = Lexer::new(input);
+        let tokens = command_lexer.tokenize();
+
+        assert_eq!(tokens, expected_tokens);
+    }
+
+    #[test]
+    fn tokenize_two_arguments() {
+        let input = "mv file1.png folder1/file1.png";
+
+        let expected_tokens = vec![
+            Token::Word(String::from("mv")),
+            Token::Space,
+            Token::Word(String::from("file1")),
+            Token::Dot,
+            Token::Word(String::from("png")),
+            Token::Space,
+            Token::Word(String::from("folder1")),
+            Token::Slash,
+            Token::Word(String::from("file1")),
+            Token::Dot,
+            Token::Word(String::from("png")),
+        ];
+
+        let mut command_lexer = Lexer::new(input);
         let tokens = command_lexer.tokenize();
 
         assert_eq!(tokens, expected_tokens);
